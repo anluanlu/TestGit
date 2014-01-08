7ed5bfff53d79c03c82294dff8960fe63c582f1b
anluanlu123@163.com <anluanlu123@163.com>
2014-01-08 10:03:37 +0800

    test
    test

---
+++ GuildBrowser_Starter/Test1/Test1.h
@@ -0,0 +1,13 @@
+//
+//  Test1.h
+//  Test1
+//
+//  Created by Andrew on 14-1-7.
+//  Copyright (c) 2014年 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Test1 : SenTestCase
+
+@end
---
+++ GuildBrowser_Starter/Test1/Test1.m
@@ -0,0 +1,32 @@
+//
+//  Test1.m
+//  Test1
+//
+//  Created by Andrew on 14-1-7.
+//  Copyright (c) 2014年 Charlie Fulton. All rights reserved.
+//
+
+#import "Test1.h"
+
+@implementation Test1
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Test1");
+}
+
+@end
