a321c832c14ee19540142d14e996c2f52b8e968c
anluanlu123@163.com <anluanlu123@163.com>
2014-01-08 14:14:52 +0800

    提交代码
    添加了一个新文件

---
+++ GuildBrowser_Starter/GuildBrowser/Test1.h
@@ -0,0 +1,13 @@
+//
+//  Test1.h
+//  GuildBrowser
+//
+//  Created by Andrew on 14-1-8.
+//  Copyright (c) 2014年 Charlie Fulton. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test1 : NSObject
+
+@end
---
+++ GuildBrowser_Starter/GuildBrowser/Test1.m
@@ -0,0 +1,13 @@
+//
+//  Test1.m
+//  GuildBrowser
+//
+//  Created by Andrew on 14-1-8.
+//  Copyright (c) 2014年 Charlie Fulton. All rights reserved.
+//
+
+#import "Test1.h"
+
+@implementation Test1
+
+@end
